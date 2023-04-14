81edf129f2b83703c4bb761ba436cf1c9f42dd01
sda782 <saerdna.petersen@gmail.com>
2023-04-14 13:54:58 +0200

    stuff

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,9 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
-{
-public Action LevelComplete;
+public class LevelManager : MonoBehaviour {
+    public Action<GameObject> LevelComplete;
+    private void Start() {
+        LevelComplete += OnLevelComplete;
+    }
+    private void OnLevelComplete(GameObject checkPoint) {
+        checkPoint.SetActive(false);
+    }
+
 }
